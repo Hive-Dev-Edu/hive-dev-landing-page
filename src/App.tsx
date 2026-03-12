6a09cbb04fc119758ae49c4d1b4c786cd0fc6b78
camiwr <camillasoares818@gmail.com>
2026-03-12 17:36:26 -0300

    feat: add new Radix UI components including Menubar, Navigation Menu, Popover, Progress, Radio Group, Scroll Area, Select, and Separator; update imports and remove NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "./components/ui/tooltip";
-import Index from "./pages";
-import NotFound from "./pages";
+import Index from "./pages/index";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +16,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
